cbb500d03c6bd33eab7820d1c9e940ac4570db69
Tahabakk <tahabakk2@gmail.com>
2017-01-07 22:53:49 +0100

    Capturamos en una variable los datos que se introducen en el editText y el Spinner

--- app/src/main/java/com/bakk/taha/leagueoflegends/MainActivityFragment.java
+++ app/src/main/java/com/bakk/taha/leagueoflegends/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class MainActivityFragment extends Fragment {
@@ -16,11 +20,36 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+    private EditText et1;
+    private Spinner sp1;
+
+    private ImageButton masVida1;
+    private TextView textView1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        et1 = (EditText) view.findViewById(R.id.editText1);
+        sp1 = (Spinner) view.findViewById(R.id.lista1);
+
+        String datoNombre = et1.getText().toString();//con esto capturamos el texto introducido
+        String datoServidor = sp1.getItemAtPosition(sp1.getSelectedItemPosition()).toString();//capturamos el servidor seleccionado en el Spinner
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
